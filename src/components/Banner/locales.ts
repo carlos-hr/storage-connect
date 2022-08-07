d66c9057a2410a0a463cf6186e87d84dc5f354b9
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-08-07 18:02:44 -0300

    adjusting locales text

--- src/components/Banner/locales.ts
+++ src/components/Banner/locales.ts
@@ -1,3 +1,5 @@
 export const bannerTitle = "Self Storage Business Buy. Sell. Learn.";
 export const bannerSubtitle =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit sagittis, aliquam non tortor blandit lobortis. blandit ";
+export const firstButton = "Sell Your Self Storage";
+export const secondButton = "Active Self Storage Listings";
